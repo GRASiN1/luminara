d8a28ea7cb9f8cc5d931cb853c7ff29907c17820
GRASiN1 <gauravrajsingh047@gmail.com>
2024-08-16 10:57:02 +0530

    added controller function to routes

--- backend/routes/address.js
+++ backend/routes/address.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const { handleCreateAddress, handleUpdateAddressById, handleGetAllAddressOfUser } = require('../controller/address');
 
 const router = Router();
 
-router.post('/createAddress',);
-router.post('/updateAddress',);
-router.get('/getAllAddressOfUser',);
+router.post('/createAddress', handleCreateAddress);
+router.post('/updateAddressById/:id', handleUpdateAddressById);
+router.get('/getAllAddressOfUser', handleGetAllAddressOfUser);
 
 module.exports = router;
