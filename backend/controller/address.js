aea9a992fa9c3ed115cf4290d6e219cba1106c82
GRASiN1 <gauravrajsingh047@gmail.com>
2024-08-16 10:56:44 +0530

    created controller functions for address CRUD

---
+++ backend/controller/address.js
@@ -0,0 +1,57 @@
+const Address = require('../models/address');
+
+async function handleCreateAddress(req, res) {
+    const { fullName, email, phoneNumber, fullAddress } = req.body;
+    const address = await Address.create({
+        fullName: fullName,
+        email: email,
+        phoneNumber: phoneNumber,
+        fullAddress: fullAddress,
+        addressOf: req.user._id,
+    })
+    if (address) {
+        return res.status(200).json(address);
+    } else {
+        return res.status(400).json({ message: 'error creating address' });
+    }
+}
+
+async function handleGetAllAddressOfUser(req, res) {
+    const user = req.user._id;
+    const addressList = await Address.find({ addressOf: user });
+    if (addressList.length !== 0) {
+        return res.status(200).json(addressList);
+    } else {
+        return res.status(400).json({ message: 'user have no address' });
+    }
+}
+
+async function handleUpdateAddressById(req, res) {
+    const addressToBeChangedId = req.params.id;
+    const { fullName, email, phoneNumber, fullAddress } = req.body;
+    try {
+        const address = await Address.find({ addressToBeChangedId });
+        if (address) {
+            address.fullName = fullName || address.fullName;
+            address.fullAddress = fullAddress || address.fullAddress;
+            address.email = email || address.email;
+            address.phoneNumber = phoneNumber || address.phoneNumber;
+            const updatedAddress = await address.save();
+            return res.status(200).json({
+                message: 'address updated',
+                updatedAddress
+            });
+        } else {
+            return res.status(400).json({ message: 'No address by this id' });
+
+        }
+    } catch (error) {
+        return res.status(400).json({ message: 'error updating address' });
+    }
+}
+
+module.exports = {
+    handleCreateAddress,
+    handleGetAllAddressOfUser,
+    handleUpdateAddressById,
+}
