c38b9ec013133a080d78359b28365120a37432f0
GRASiN1 <gauravrajsingh047@gmail.com>
2024-08-16 11:42:18 +0530

    created controller functions for review routes

---
+++ backend/controller/review.js
@@ -0,0 +1,39 @@
+const Review = require('../models/review');
+const Product = require('../models/product');
+
+async function handleCreateReview(req, res) {
+    const { rating, comment } = req.body;
+    const product = await Product.find({ _id: req.params.productId });
+    if (product) {
+        const alreadyReviewed = product.reviews.find(
+            (r) => r.reviewBy.toString() === req.user._id.toString()
+        );
+        if (alreadyReviewed) {
+            return res.status(400).json({ message: 'Already reviewed by current user' });
+        }
+        await Review.create({
+            reviewBy: req.user._id,
+            reviewBody: comment,
+            stars: rating,
+            reviewOf: req.params.productId,
+        })
+        return res.status(200).json({ message: 'review added' });
+    } else {
+        return res.status(400).json({ message: 'No product by this id' });
+    }
+}
+
+async function handleGetAllReViewsOfProduct(req, res) {
+    const productId = req.params.productId;
+    const reviewList = await Review.find({ reviewOf: productId });
+    if (reviewList.length !== 0) {
+        return res.status(200).json(reviewList);
+    } else {
+        return res.status(400).json({ message: 'No reviews on this product' });
+    }
+}
+
+module.exports = {
+    handleCreateReview,
+    handleGetAllReViewsOfProduct
+}
