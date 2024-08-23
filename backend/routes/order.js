82e9806d1a5c93adf524cdfe43e65f77b40148b7
GRASiN1 <gauravrajsingh047@gmail.com>
2024-08-23 12:04:40 +0530

    completed order route

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
+const { restrictTo } = require('../middleware/authorization')
+const { hanldeGetUserOrder, handleCreateOrder, handleGetAllOrders, handleUpadteOrderStatus } = require("../controller/order");
 
 const router = Router();
 
-router.post('/createOrder',);
-router.get('/getUserOrder',);
-router.get('/getAllOrders',);
-router.post('/updateOrderStatus',);
+router.post('/createOrder', handleCreateOrder);
+router.get('/getUserOrder', hanldeGetUserOrder);
+router.get('/getAllOrders', restrictTo(['ADMIN']), handleGetAllOrders);
+router.post('/updateOrderStatusById/:id', restrictTo(['ADMIN']), handleUpadteOrderStatus);
 
 module.exports = router;
